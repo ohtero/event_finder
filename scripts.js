cdfe158f686c4cd52230fe72917b6e01c0917dc7
ohtero <91533120+Tuhto@users.noreply.github.com>
2022-11-24 19:22:54 +0200

    testing

--- scripts.js
+++ scripts.js
@@ -211,7 +211,7 @@ document.getElementById('show-50').addEventListener('click', changeShownItemCoun
 
 function fetchData() {
     const getEvents = new XMLHttpRequest;
-    getEvents.open('GET', `https://open-api.myhelsinki.fi/v1/events/?tags_filter=${encodedSearchStr}`, true);
+    getEvents.open('GET', `http://open-api.myhelsinki.fi/v1/events/?tags_filter=${encodedSearchStr}`, true);
     getEvents.setRequestHeader('access-control-allow-headers', 'origin');
     getEvents.setRequestHeader('access-control-allow-methods', 'PUT, GET, HEAD, POST, DELETE, OPTIONS ');
     getEvents.setRequestHeader('access-control-allow-origin', '*');
@@ -231,7 +231,7 @@ function fetchData() {
 
 function showData(index = 0) {
     const xhr = new XMLHttpRequest;
-    xhr.open('GET', `https://open-api.myhelsinki.fi/v1/events/?tags_filter=${encodedSearchStr}&limit=${shownItemsCount}&start=${index}`, true);
+    xhr.open('GET', `http://open-api.myhelsinki.fi/v1/events/?tags_filter=${encodedSearchStr}&limit=${shownItemsCount}&start=${index}`, true);
     xhr.setRequestHeader('access-control-allow-headers', 'origin');
     xhr.setRequestHeader('access-control-allow-methods', 'PUT, GET, HEAD, POST, DELETE, OPTIONS ');
     xhr.setRequestHeader('access-control-allow-origin', '*');
