b6a4d5aac47cad03dc3a1520329232f19665002f
ohtero <91533120+Tuhto@users.noreply.github.com>
2022-11-24 19:56:11 +0200

    testing

--- scripts.js
+++ scripts.js
@@ -212,16 +212,17 @@ document.getElementById('show-50').addEventListener('click', changeShownItemCoun
 function fetchData() {
     const getEvents = new XMLHttpRequest;
     getEvents.open('GET', `https://open-api.myhelsinki.fi/v1/events/?tags_filter=${encodedSearchStr}`, true);
-    getEvents.setRequestHeader('access-control-allow-methods', 'PUT, GET, HEAD, POST, DELETE, OPTIONS ');
-    getEvents.setRequestHeader('access-control-allow-origin', '*');
-    xhr.setRequestHeader('access-control-allow-headers', '*');
-    getEvents.setRequestHeader('connection', 'keep-alive');
-    getEvents.setRequestHeader('content-length', '17641');
-    getEvents.setRequestHeader('content-type', 'application/json');
-    getEvents.setRequestHeader('referrer-policy', 'origin-when-cross-origin, strict-origin-when-cross-origin ');
+    // getEvents.setRequestHeader('access-control-allow-methods', 'PUT, GET, HEAD, POST, DELETE, OPTIONS ');
+    // getEvents.setRequestHeader('access-control-allow-origin', '*');
+    // getEvents.getResponseHeader('access-control-allow-origin');
+    // getEvents.setRequestHeader('access-control-allow-headers', '*');
+    // getEvents.setRequestHeader('content-type', 'application/json');
+
+
     getEvents.onload = function count() {
         if (this.status == 200) {
             const res = JSON.parse(this.response);
+
             CountEvents(res);
             getTags(res);
         };        
@@ -235,18 +236,10 @@ function fetchData() {
 function showData(index = 0) {
     const xhr = new XMLHttpRequest;
     xhr.open('GET', `https://open-api.myhelsinki.fi/v1/events/?tags_filter=${encodedSearchStr}&limit=${shownItemsCount}&start=${index}`, true);
-    xhr.setRequestHeader('access-control-allow-methods', 'PUT, GET, HEAD, POST, DELETE, OPTIONS ');
-    xhr.setRequestHeader('access-control-allow-origin', '*');
-    xhr.setRequestHeader('access-control-allow-headers', '*');
-    xhr.setRequestHeader('connection', 'keep-alive');
-    xhr.setRequestHeader('content-length', '17641');
-    xhr.setRequestHeader('content-type', 'application/json');
-    xhr.setRequestHeader('referrer-policy', 'origin-when-cross-origin, strict-origin-when-cross-origin ');
+    // xhr.setRequestHeader('access-control-allow-methods', 'PUT, GET, HEAD, POST, DELETE, OPTIONS ');
+    // xhr.getResponseHeader('access-control-allow-origin');
+    // xhr.setRequestHeader('access-control-allow-headers', '*');
     // xhr.setRequestHeader('content-type', 'application/json');
-    // xhr.setRequestHeader('content-type', 'application/json');
-    // xhr.setRequestHeader('content-type', 'application/json');
-    // xhr.setRequestHeader('content-type', 'application/json');
-    
     xhr.onload = function show() {
         if (this.status == 200) {
             const res = JSON.parse(this.response);
